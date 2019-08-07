3eb8bb7b376c8a9134b63798020ed549ff49a62c
zidol <ajh3166@naver.com>
2019-08-07 23:35:24 +0900

    SpringBoot Security 설정 및 테스트

---
+++ src/main/java/me/zidol/sprongbootsecurity/SprongbootsecurityApplication.java
@@ -0,0 +1,13 @@
+package me.zidol.sprongbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprongbootsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprongbootsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/zidol/sprongbootsecurity/SprongbootsecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package me.zidol.sprongbootsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprongbootsecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
